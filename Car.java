c9547016f7babd8ede4b2c3c646f57efa2992afe
UBER60 <rcho5318@gmail.com>
2024-09-10 15:36:16 -0700

    Update Car.java
    not doen

--- Car.java
+++ Car.java
@@ -1,1 +1,19 @@
+public class Car{
+  private String carName;
+  private double mileDriven;
+  private double gallonsUsed;
 
+  public Car(){
+    carName = "";
+    mileDriven = 0;
+    gallonsUsed = 0;
+  }
+  public Car(String newCarName, double newMileDriven, double newGallonsUsed){
+    carName = newCarName;
+    MileDriven = newMileDriven;
+    GallonsUsed = newGallonsUsed;
+  }
+  public String getCarName(){
+    return carName
+  }
+}
