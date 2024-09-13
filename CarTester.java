4807e8de4e3bfa007b48a49ddd19944fdab8f5f5
UBER60 <rcho5318@gmail.com>
2024-09-13 11:59:25 +0800

    Update CarTester.java

--- CarTester.java
+++ CarTester.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
-public class CarTester(){
+public class CarTester{
   public Car addCar(){
     Scanner x = new Scanner(System.in);
     Car a = new Car();
-    System.out.print("Please enter the car's name: ")
-    a.getCarName(x.nextString());
-    System.out.println();
-    System.out.print("Please enter cars miles: ")
-    a.getMilesDriven(x.nextDouble());
-    System.out.println();
-    System.out.print("Please enter cars gallons: ")
-    a.getGallonsUsed(x.nextDouble());
-    System.out.println();
+    System.out.print("Please enter the car's name: ");
+    a.setCarName(x.next());
+    System.out.print("Please enter cars miles: ");
+    a.setMileDriven(x.nextDouble());
+    System.out.print("Please enter cars gallons: ");
+    a.setGallonsUsed(x.nextDouble());
     return a;
-  public static void(String[] args){
+  }
+  public static void main(String args[]){
     CarTester s = new CarTester();
-    Car a1 = new s.addCar();
-    a1.toString();
-    Car a2 = new s.addCar();
-    a2.toString();
+    Car a1 = s.addCar();
+    System.out.println(a1.toString());
+    Car a2 = s.addCar();
+    System.out.println(a2.toString());
   }
 }
